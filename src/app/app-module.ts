d2cdf57991b14b7a9c98b7a1b7392e5c235ec85f
Camila Kfouri <kfouri.camila@gmail.com>
2025-07-30 09:56:42 +0100

    feat: add Course module with type definition, interface, and routing setup

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from '../shared/angular-material/angular-material-module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { App } from './app';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners()
---
+++ src/app/courses/courses-module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CoursesRoutingModule } from './courses-routing-module';
+import { AngularMaterialModule } from '../../shared/angular-material/angular-material-module';
+import { Courses } from './courses/courses';
+
+
+@NgModule({
+  declarations: [
+    Courses
+  ],
+  imports: [
+    CommonModule,
+    CoursesRoutingModule,
+    AngularMaterialModule
+  ]
+})
+export class CoursesModule { }
